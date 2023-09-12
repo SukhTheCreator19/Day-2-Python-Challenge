f58f2ff62427a656d82bc8aecc44d76815c8dc6c
SukhTheCreator19 <143115777+SukhTheCreator19@users.noreply.github.com>
2023-09-12 17:54:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import math
+name = input("What is your full name: ")
+sales = float(input("What was your total amount of sales this month: "))
+commision = (sales * 13)/ 100
+print(f' Hello {name}, this month you have sold ${sales} of merchandise, you are entitled to 13%, so your commision will be ${commision} ')
